19be7fa6c887e13247fda108f313d8ae147ee1f0
许强强 <121837631@qq.com>
2019-06-18 15:34:58 +0800

    mapping

--- src/main/java/com/cirvor/learn/config/ExceptionHandlerConfig.java
+++ src/main/java/com/cirvor/learn/config/ExceptionHandlerConfig.java
@@ -2,17 +2,31 @@ package com.cirvor.learn.config;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionHandlerConfig  extends ResponseEntityExceptionHandler {
+    /**
+     * 处理Exception异常
+     * @param e
+     * @return
+     */
     @ExceptionHandler(value = Exception.class)
     public Object handleException(Exception e) {
         Map<String, Object> result = new HashMap<>();
-        result.put("code", 500);
-        result.put("msg", e.getMessage());
+
+        if (e instanceof NoHandlerFoundException) {
+            System.out.println(111111);
+            result.put("code", 404);
+            result.put("msg", e.getMessage());
+        } else {
+            result.put("code", 500);
+            result.put("msg", e.getMessage());
+        }
+
         return result;
     }
 }
